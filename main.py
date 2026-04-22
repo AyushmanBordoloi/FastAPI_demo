66f655810ccfdafa46c8013064d7885a8bd6efe7
AyushmanBordoloi <ayushmanbordoloi02@gmail.com>
2026-04-22 23:58:24 +0530

    add data validation using pydantic

--- main.py
+++ main.py
@@ -8,8 +8,10 @@ def greet():
     return "Welcome to my server"
 
 products = [
-    Product(1, "Phone", "budget phone", 99, 10),
-    Product(2, "Laptop", "budget laptop", 999, 6),
+    Product(pid=1, name="Phone", description="budget phone", price=99, quantity=10),
+    Product(pid=2, name="Laptop", description="budget laptop", price=999, quantity=6),
+    Product(pid=3, name="Headphones", description="budget headphones", price=49, quantity=15),
+    Product(pid=4, name="Smartwatch", description="budget smartwatch", price=199, quantity=8),
 ]
 
 @app.get("/products")
--- models.py
+++ models.py
@@ -1,15 +1,8 @@
 from pydantic import BaseModel
 
-class Product:
+class Product(BaseModel):
     pid: int
     name: str
     description: str
     price: float
     quantity: int
-
-    def __init__(self, pid: int, name: str, description: str, price: float, quantity: int):
-        self.pid = pid
-        self.name = name
-        self.description = description
-        self.price = price
-        self.quantity = quantity
